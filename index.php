72054a90d7d2e4cf433b8f365cc9e13fe71d0b29
Marco <mposadas1979@gmail.com>
2023-02-23 07:43:23 -0600

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" type="image/jpg" href="public/img/handsome.jpg">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Agenda de contactos web</title>
+    <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+    
+</body>
+</html>
